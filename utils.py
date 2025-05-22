b594a8889056241e7e289ca7ef9de9a38530f0fe
Emilia272539 <272539@student.pwr.edu.pl>
2025-05-22 21:49:07 +0200

    black

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 """Moduł z podstawowymi funkcjami matematycznymi do testów CI/CD."""
 
+
 def add(a: int, b: int) -> int:
     """Zwraca sumę a i b."""
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Zwraca różnicę a - b."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     """Zwraca iloczyn a i b."""
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Zwraca iloraz a / b."""
